781f45599b9d19739f611503522c876a08db63e8
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-09-15 22:38:30 -0300

    Exemplo obter por id

--- tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/spring/controller/ProdutoController.java
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/spring/controller/ProdutoController.java
@@ -10,6 +10,7 @@ import br.senac.tads4.dsw.tadsstore.common.service.ProdutoService;
 import br.senac.tads4.dsw.tadsstore.common.service.fakeimpl.ProdutoServiceFakeImpl;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,14 +23,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
-  
+
+  private ProdutoService service = new ProdutoServiceFakeImpl();
+
   @RequestMapping(method = RequestMethod.GET)
   public ModelAndView listar(
-	  @RequestParam(value = "offset", required = false, defaultValue = "0") int offset, 
+	  @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
 	  @RequestParam(value = "qtd", required = false, defaultValue = "20") int qtd) {
-    ProdutoService service = new ProdutoServiceFakeImpl();
     List<Produto> itens = service.listar(offset, qtd);
     return new ModelAndView("/produto/lista").addObject("itens", itens);
   }
-  
+
+  @RequestMapping(value = "/{id:[0-9]+}", method = RequestMethod.GET)
+  public ModelAndView obter(@PathVariable("id") Long id) {
+    Produto p = service.obter(id);
+    return new ModelAndView("produto/detalhe").addObject("produto", p);
+  }
+
 }
